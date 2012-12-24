785fa6d6c8e59e5490f1b561d9818c78eb9aa3b1
zhangxi <zhangxi_1989@sina.com>
2012-12-24 16:16:05 +0800

    init

---
+++ UIView+ZXQuartz.h
@@ -0,0 +1,17 @@
+//
+//  UIView+ZXQuartz.h
+//  quartz
+//
+//  Created by 张 玺 on 12-12-24.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ZXQuartz)
+
+
+-(CGMutablePathRef)pathwithFrame:(CGRect)frame withRadius:(float)radius;
+
+
+@end
---
+++ UIView+ZXQuartz.m
@@ -0,0 +1,69 @@
+//
+//  UIView+ZXQuartz.m
+//  quartz
+//
+//  Created by 张 玺 on 12-12-24.
+//  Copyright (c) 2012年 张玺. All rights reserved.
+//
+
+#import "UIView+ZXQuartz.h"
+
+@implementation UIView (ZXQuartz)
+
+-(CGMutablePathRef)pathwithFrame:(CGRect)frame withRadius:(float)radius
+{
+    CGPoint x1,x2,x3,x4; //x为4个顶点
+    CGPoint y1,y2,y3,y4,y5,y6,y7,y8; //y为4个控制点
+    //从左上角顶点开始，顺时针旋转,x1->y1->y2->x2
+    
+    x1 = frame.origin;
+    x2 = CGPointMake(frame.origin.x+frame.size.width, frame.origin.y);
+    x3 = CGPointMake(frame.origin.x+frame.size.width, frame.origin.y+frame.size.height);
+    x4 = CGPointMake(frame.origin.x                 , frame.origin.y+frame.size.height);
+    
+    
+    y1 = CGPointMake(frame.origin.x+radius, frame.origin.y);
+    y2 = CGPointMake(frame.origin.x+frame.size.width-radius, frame.origin.y);
+    y3 = CGPointMake(frame.origin.x+frame.size.width, frame.origin.y+radius);
+    y4 = CGPointMake(frame.origin.x+frame.size.width, frame.origin.y+frame.size.height-radius);
+    
+    y5 = CGPointMake(frame.origin.x+frame.size.width-radius, frame.origin.y+frame.size.height);
+    y6 = CGPointMake(frame.origin.x+radius, frame.origin.y+frame.size.height);
+    y7 = CGPointMake(frame.origin.x, frame.origin.y+frame.size.height-radius);
+    y8 = CGPointMake(frame.origin.x, frame.origin.y+radius);
+    
+    
+    CGMutablePathRef pathRef = CGPathCreateMutable();
+    
+    if (radius<=0) {
+        CGPathMoveToPoint(pathRef,    &CGAffineTransformIdentity, x1.x,x1.y);
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, x2.x,x2.y);
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, x3.x,x3.y);
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, x4.x,x4.y);
+    }else
+    {
+        CGPathMoveToPoint(pathRef,    &CGAffineTransformIdentity, y1.x,y1.y);
+        
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, y2.x,y2.y);
+        CGPathAddArcToPoint(pathRef, &CGAffineTransformIdentity,  x2.x,x2.y,y3.x,y3.y,radius);
+        
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, y4.x,y4.y);
+        CGPathAddArcToPoint(pathRef, &CGAffineTransformIdentity,  x3.x,x3.y,y5.x,y5.y,radius);
+        
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, y6.x,y6.y);
+        CGPathAddArcToPoint(pathRef, &CGAffineTransformIdentity,  x4.x,x4.y,y7.x,y7.y,radius);
+        
+        CGPathAddLineToPoint(pathRef, &CGAffineTransformIdentity, y8.x,y8.y);
+        CGPathAddArcToPoint(pathRef, &CGAffineTransformIdentity,  x1.x,x1.y,y1.x,y1.y,radius);
+        
+    }
+    
+    
+    CGPathCloseSubpath(pathRef);
+    
+    [[UIColor whiteColor] setFill];
+    [[UIColor blackColor] setStroke];
+    
+    return pathRef;
+}
+@end
